4e2237c7151dc4c9fb407ceb81348c3cd94097f3
sheng_lu <sheng_lu@yunzhijia.com>
2020-08-08 02:33:29 +0800

    leetcode03（最长不重复子串）： 新增官方版本

--- src/test03_lengthOfLongestSubstring/LengthOfLongestSubstring.java
+++ src/test03_lengthOfLongestSubstring/LengthOfLongestSubstring.java
@@ -1,11 +1,13 @@
 package test03_lengthOfLongestSubstring;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 /**
  * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
@@ -30,12 +32,13 @@ import java.util.List;
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ *
+ * #################################
+ *  官方，滑动窗口
+ *
  */
 public class LengthOfLongestSubstring {
-    public static void main(String[] args) {
-        int cdd = lengthOfLongestSubstring_v2("abcabcbb");
-        System.out.println(cdd);
-    }
     /**
      * version1
      * @param s
@@ -71,37 +74,37 @@ public class LengthOfLongestSubstring {
     }
 
     /**
-     * 版本2
-     * @param s
-     * @return
+     * 官方版本（滑动窗口+set）
+     *
+     * 复杂度分析
+     *
+     * 时间复杂度：O(N)O(N)，其中 NN 是字符串的长度。左指针和右指针分别会遍历整个字符串一次。
+     *
+     * 空间复杂度：O(|\Sigma|)O(∣Σ∣)，其中 \SigmaΣ 表示字符集（即字符串中可以出现的字符），|\Sigma|∣Σ∣ 表示字符集的大小。
+     * 在本题中没有明确说明字符集，因此可以默认为所有 ASCII 码在 [0, 128)[0,128) 内的字符，即 |\Sigma| = 128∣Σ∣=128。
+     * 我们需要用到哈希集合来存储出现过的字符，而字符最多有 |\Sigma|∣Σ∣ 个，因此空间复杂度为 O(|\Sigma|)O(∣Σ∣)。
+     *
      */
-    public static int lengthOfLongestSubstring_v2(String s) {
-        /**
-         * 思路：
-         *
-         *
-         */
-        LinkedHashSet<Character> set = new LinkedHashSet<>();
-        int max = 0;
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if(set.contains(c)){
-                Iterator<Character> iterator = set.iterator();
-                int remove = 1;
-                while(iterator.hasNext()){
-                    Character character = iterator.next();
-                    if(character.equals(c)){
-                        max = max>remove?max:remove;
-                        iterator.remove();
-                    }else{
-                        iterator.remove();
-                    }
-                    remove++;
-                }
+    public int lengthOfLongestSubstring(String s) {
+        // 哈希集合，记录每个字符是否出现过
+        Set<Character> occ = new HashSet<Character>();
+        int n = s.length();
+        // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+        int rk = -1, ans = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i != 0) {
+                // 左指针向右移动一格，移除一个字符
+                occ.remove(s.charAt(i - 1));
             }
-            set.add(c);
+            while (rk + 1 < n && !occ.contains(s.charAt(rk + 1))) {
+                // 不断地移动右指针
+                occ.add(s.charAt(rk + 1));
+                ++rk;
+            }
+            // 第 i 到 rk 个字符是一个极长的无重复字符子串
+            ans = Math.max(ans, rk - i + 1);
         }
-        max = max>set.size()?max:set.size();
-        return max;
+        return ans;
     }
+
 }
